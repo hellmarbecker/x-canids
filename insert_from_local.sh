70151e119af5ee891ba4780fb65fee2cd2d62c41
Hellmar Becker <becker@hellmar-becker.de>
2025-10-16 18:34:57 +0200

    Insert data from arbitration_id.csv into ClickHouse
    Add functionality to insert data from arbitration_id.csv.

--- insert_from_local.sh
+++ insert_from_local.sh
@@ -11,3 +11,8 @@ echo $i
 ./clickhouse local -q "SELECT *,_file FROM file('$i')" --format Native | \
 ./clickhouse  client --host mqvsbo7uzs.eu-central-1.aws.clickhouse.cloud --port 9440 --user default --password 'PASSWORD' -q 'INSERT INTO xcanids.signal FORMAT Native'
 done
+
+
+./clickhouse local -q "SELECT * from file('./arbitration_id.csv', CSVWithNames)"  --format Native | \
+./clickhouse  client --host mqvsbo7uzs.eu-central-1.aws.clickhouse.cloud --port 9440 --user default --password 'PASSWORD' -q 'INSERT INTO arbitration_id FORMAT Native'
+
